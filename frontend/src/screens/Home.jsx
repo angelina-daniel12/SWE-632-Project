ed186095f5b910354405dec7c94459328bc171ef
turkmenkaan <kaanturkmen2000@gmail.com>
2026-01-30 22:20:42 -0500

    add rank modal base

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,10 +1,16 @@
 
-
+import RankModal from "../components/RankModal";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 function Home() {
+    const [open, setOpen] = useState(false);
+
     return (
         <div>
             <h1>Home</h1>
+            <Button onClick={() => setOpen(true)}>Open Modal</Button>
+            <RankModal open={true} handleClose={() => setOpen(false)} />
         </div>
     )
 }
