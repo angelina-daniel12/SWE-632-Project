ae26f78ed85b193fa36eaf710753ae8d7ce64bf9
turkmenkaan <kaanturkmen2000@gmail.com>
2026-02-02 19:29:43 -0500

    update endpoint with https

--- react-app/src/contexts/ApiContext.js
+++ react-app/src/contexts/ApiContext.js
@@ -7,8 +7,13 @@ const ApiContext = createContext();
 
 export function ApiProvider({ children }) {
     // Swap between production and local server URLs as needed
+<<<<<<< HEAD
     // const SERVER_URL_PROXY = 'https://interadditive-benny-matrilineal.ngrok-free.dev';
     // const SERVER_URL_PROD = 'http://139.144.222.250:8000';
+=======
+    // const SERVER_URL_PROD = 'https://interadditive-benny-matrilineal.ngrok-free.dev';
+    const SERVER_URL = 'https://metatier.turkmenkaan.xyz:8000'
+>>>>>>> 7d6f248 (update endpoint with https)
     // const SERVER_URL_LOCAL = 'http://localhost:8000';
     const SERVER_URL_PROD = "https://metatier.turkmenkaan.xyz:8000"
     const SERVER_URL = SERVER_URL_PROD;
